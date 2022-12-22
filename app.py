68ef70c740c1e3a1af3e85765f52936f4b0c7859
RuslanMirov <ruslan777mirov@gmail.com>
2022-12-22 20:48:42 +0200

    Update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 import converter
+from PIL import Image
 
 app = Flask(__name__)
 
@@ -16,9 +17,15 @@ def upload_image():
     if file.filename == '':
         return render_template('upload.html', message='No file selected')
     if file:
+        # load image
         filename = secure_filename(file.filename)
-        # file.save(f'uploads/{filename}')
-        converter.convert(file)
+        file.save(f'uploads/{filename}')
+
+        # convert
+        image = Image.open(f'uploads/{filename}')
+        converter.convert(image)
+
+        # return
         return render_template('upload.html', message='File uploaded successfully', file=filename)
 
 if __name__ == '__main__':
